b7c7b6e8ba72e44c346f124bd97e8b53b42c9341
Arvind_Prakash_T <arvindprakasht23@gmail.com>
2026-02-22 01:01:15 +0530

    Update parser.c

--- parser.c
+++ parser.c
@@ -1,8 +1,9 @@
+#include <string.h>
 #include "parser.h"
 #include "axi_regs.h"
 #include "api.h"
-#include <string.h>
 
+/* Custom hex parser to handle hex strings (e.g., "0x1A" or "1A") */
 static u32 parse_hex(const char *s)
 {
     u32 val = 0;
@@ -31,36 +32,58 @@ static u32 parse_hex(const char *s)
     return val;
 }
 
-void parse_and_issue(char *cmd)
+void parse_and_store(char *cmd)
 {
-    char *tok = strtok(cmd, " ");
-    u32 operands[MAX_OPERANDS];
-    int count = 0;
+    /* Block new commands if currently busy */
+    if (READ8(REG_COMMAND) != 0) return;
 
+    char *tok = strtok(cmd, " ");
     if (!tok) return;
 
-    u8 opcode = 0xFF;
-
-    if (!strcmp(tok, "READ"))
-        opcode = OPCODE_READ;
-    else if (!strcmp(tok, "WRITE"))
-        opcode = OPCODE_WRITE;
+    if (!strcmp(tok, "WRITE"))
+    {
+        WRITE8(REG_OPCODE, OPCODE_WRITE);
+        char *addr = strtok(NULL, " ");
+        char *val = strtok(NULL, " ");
+        if (addr) WRITE32(REG_OPERAND(0), parse_hex(addr));
+        if (val)  WRITE32(REG_OPERAND(1), parse_hex(val));
+    }
+    else if (!strcmp(tok, "READ"))
+    {
+        WRITE8(REG_OPCODE, OPCODE_READ);
+        char *addr = strtok(NULL, " ");
+        if (addr) WRITE32(REG_OPERAND(0), parse_hex(addr));
+    }
     else if (!strcmp(tok, "ARRAY_WRITE"))
-        opcode = OPCODE_ARRAY_WRITE;
+    {
+        WRITE8(REG_OPCODE, OPCODE_ARRAY_WRITE);
+        char *addr = strtok(NULL, " ");
+        char *len = strtok(NULL, " ");
+        if (addr && len) 
+        {
+            WRITE32(REG_OPERAND(0), parse_hex(addr));
+            u32 length = parse_hex(len);
+            
+            /* Limit length to 6 to fit in Operands[2..7] */
+            if (length > 6) length = 6; 
+            WRITE32(REG_OPERAND(1), length);
+            
+            for (u32 i = 0; i < length; i++) {
+                char *data = strtok(NULL, " ");
+                if (data) WRITE32(REG_OPERAND(2 + i), parse_hex(data));
+                else break;
+            }
+        }
+    }
     else if (!strcmp(tok, "ARRAY_READ"))
-        opcode = OPCODE_ARRAY_READ;
+    {
+        WRITE8(REG_OPCODE, OPCODE_ARRAY_READ);
+        char *addr = strtok(NULL, " ");
+        char *len = strtok(NULL, " ");
+        if (addr) WRITE32(REG_OPERAND(0), parse_hex(addr));
+        if (len)  WRITE32(REG_OPERAND(1), parse_hex(len));
+    }
 
-    if (opcode == 0xFF)
-        return;
-
-    while ((tok = strtok(NULL, " ")) && count < MAX_OPERANDS)
-        operands[count++] = parse_hex(tok);
-
-    for (int i = 0; i < count; i++)
-        WRITE32(REG_OPERAND(i), operands[i]);
-
-    WRITE8(REG_OPCODE_ADDR, opcode);
-
-    /* Trigger execution */
-    WRITE8(REG_COMMAND_ADDR, CMD_START);
+    /* Start execution */
+    WRITE8(REG_COMMAND, 1);
 }
