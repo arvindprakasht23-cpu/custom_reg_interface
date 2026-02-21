610d7450e7d3d7c55601b260c7e2d11dc26164a2
Arvind_Prakash_T <arvindprakasht23@gmail.com>
2026-02-22 00:58:19 +0530

    Update api.h

--- api.h
+++ api.h
@@ -3,11 +3,15 @@
 
 #include "xil_types.h"
 
+/* Status Codes as defined in the TI Spec */
+#define TI_AFE_RET_EXEC_PASS 0x0000
+#define TI_AFE_RET_EXEC_FAIL 0x0001
+
 typedef u16 (*api_func_ptr)(void);
 
 typedef enum {
-    OPCODE_READ = 0,
-    OPCODE_WRITE,
+    OPCODE_WRITE = 0,
+    OPCODE_READ,
     OPCODE_ARRAY_WRITE,
     OPCODE_ARRAY_READ,
     API_TABLE_SIZE
@@ -15,4 +19,4 @@ typedef enum {
 
 extern api_func_ptr api_table[API_TABLE_SIZE];
 
-#endif
+#endif /* API_H */
